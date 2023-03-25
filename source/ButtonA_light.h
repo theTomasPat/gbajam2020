e927a73dea3e0bed1d92a64c2a26bcd36b560124
Tomas Patlan <tomas.patlan.1@gmail.com>
2023-03-25 18:34:32 -0300

    Add image data for A Button

---
+++ source/ButtonA_light.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(ButtonA_light)
+
+//======================================================================
+//
+//	ButtonA_light, 32x32@8, 
+//	+ 16 tiles not compressed
+//	Total size: 1024 = 1024
+//
+//	Time-stamp: 2023-03-11, 18:41:05
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.17
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BUTTONA_LIGHT_H
+#define GRIT_BUTTONA_LIGHT_H
+
+#define ButtonA_lightTilesLen 1024
+extern const unsigned int ButtonA_lightTiles[256];
+
+#endif // GRIT_BUTTONA_LIGHT_H
+
+//}}BLOCK(ButtonA_light)
